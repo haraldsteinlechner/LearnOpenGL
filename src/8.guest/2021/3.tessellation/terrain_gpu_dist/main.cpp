a86bc686ee83581447a2edfa3ce683ed421bffbf
Harald Steinlechner <haraldsteinlechner@gmail.com>
2024-05-23 12:08:31 +0200

    8.guest: moved tesselation heightmaps to resources, added FileSystem::getPath for loading

--- src/8.guest/2021/3.tessellation/terrain_gpu_dist/main.cpp
+++ src/8.guest/2021/3.tessellation/terrain_gpu_dist/main.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <vector>
+#include <learnopengl/filesystem.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int modifiers);
@@ -103,7 +104,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char *data = stbi_load("heightmaps/iceland_heightmap.png", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load(FileSystem::getPath("resources/heightmaps/iceland_heightmap.png").c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
@@ -205,6 +206,7 @@ int main()
         glm::mat4 model = glm::mat4(1.0f);
         tessHeightMapShader.setMat4("model", model);
 
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         // render the terrain
         glBindVertexArray(terrainVAO);
         glDrawArrays(GL_PATCHES, 0, NUM_PATCH_PTS*rez*rez);
