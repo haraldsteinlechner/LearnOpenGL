5210ee25eba7edf109103ead311a5cfad75a7d99
Harald Steinlechner <haraldsteinlechner@gmail.com>
2024-05-23 12:09:47 +0200

    Camera.h added default parameters for setting camera speed, sensitivity and zoom

--- includes/learnopengl/camera.h
+++ includes/learnopengl/camera.h
@@ -40,7 +40,7 @@ public:
     float Zoom;
 
     // constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH, float speed = SPEED, float sensitivity = SENSITIVITY, float zoom = ZOOM) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(speed), MouseSensitivity(sensitivity), Zoom(zoom)
     {
         Position = position;
         WorldUp = up;
